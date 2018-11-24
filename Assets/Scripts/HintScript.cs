47c6842e7d7dba0f1ac9af3f1bd41617edd08e25
Bernard Lekaj <bernard.lekaj@svgroup.hr>
2018-11-24 19:39:29 +0100

    Menu

---
+++ Assets/Scripts/HintScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class HintScript : DefaultTrackableEventHandler {
+
+    public string objectName = "Object";
+    
+    public Text hintText;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+
+        hintText.enabled = true;
+        hintText.text = "Found " + objectName + "!";
+
+        Invoke("HideHint", 3f);
+    }
+
+    private void HideHint()
+    {
+        hintText.enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+    
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnAboutClick()
+    {
+        SceneManager.LoadScene("AboutScene");
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+
+
+}
